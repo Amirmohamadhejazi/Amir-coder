a47a5cce1311e2f5e90399fbb5e50c54968990b9
Amirmohamad <56334460+Amirmohamadhejazi@users.noreply.github.com>
2020-10-13 19:42:18 +0330

    responsive slider for device

---
+++ src/component/page/error_page.js
@@ -0,0 +1,14 @@
+import React from "react";
+import {Link} from 'react-router-dom';
+
+
+export default function error_page() {
+
+    return (
+        <div>
+            <h1>
+                erorr 404
+            </h1>
+        </div>
+    )
+}
